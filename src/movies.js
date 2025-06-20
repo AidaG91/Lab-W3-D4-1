5ce74d9a2ea4f0758bca9f8e34b4dd7db3f7305b
AidaG91 <aida.garmus@gmail.com>
2025-06-20 12:10:31 +0200

    Iteration 5

--- src/movies.js
+++ src/movies.js
@@ -68,7 +68,18 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+  const newMoviesArray = moviesArray.slice();
+
+  newMoviesArray.sort((a, b) => {
+    if (a.year !== b.year) {
+      return a.year - b.year;
+    } else {
+      return a.title.localeCompare(b.title);
+    }
+  });
+  return newMoviesArray;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
